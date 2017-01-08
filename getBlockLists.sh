6643cae388145e801a46ad76cce0a99a0c5bb543
Casaper <vok@panter.ch>
2017-01-08 11:26:11 +0100

    allot of things, to lazy to write propper commit msg

--- getBlockLists.sh
+++ getBlockLists.sh
@@ -1,55 +1,39 @@
 #!/bin/bash
-LISTDATE=$(date +%y%m%dT%H%M%S)
-LISTS_DIR=lists_${LISTDATE}
 
-if [[ $OSTYPE =~ "darwin" ]]; then
-    TRANSMIT_CONFIG_PATH=$HOME/Library/Application\ Support/Transmission
-else
-    TRANSMIT_CONFIG_PATH=$HOME/.config/transmission
-fi
+DESTINATION_DIR="final_list"
+OUTOUT_FINAL_FILENAME="latest.p2p"
 
-TRANSMIT_BLOCKLIST_PATH=$TRANSMIT_CONFIG_PATH/blocklists
-# Download lists, unpack and filter, write to stdout
+LISTDATE=$(date +%y%m%dT%H%M%S) # DateTime string for name
+LISTS_DIR=lists_${LISTDATE}     # Temporary Dir name
+export LC_ALL='C'               # For uniq and Sort
+# Create lists directory in case its not yet there
 if [[ ! -e ./${LISTS_DIR} ]]; then
     mkdir ${LISTS_DIR}
 fi
-BLIST_SRC="$(curl -s https://www.iblocklist.com/lists.php | grep -o -e "http://list.iblocklist.com/[a-zA-Z\?\=;&0-9]*" | tr '\n' ' ')"
-for i in $BLIST_SRC; do
-    # in order to have some safe filename, grab the listname from the url
-    LISTNAME=$(echo $i | grep -o -E "list=[a-z]*" | grep -o -E "[a-z]*$")
-    wget -O ${LISTS_DIR}/${LISTNAME}.gz $i #download the list
-    gunzip ${LISTS_DIR}/${LISTNAME}.gz  #unpack the list
-    
-    # get rid of the uneccesary title with cut
-    # there will be some dummyTitle in the final output
-    cut -d ':' -s -f 2 ${LISTS_DIR}/${LISTNAME} >> ${LISTS_DIR}/cated.list 
-    rm ${LISTS_DIR}/${LISTNAME} # it is not needed anymore, so get rid or it
+# grab the lists uris from the website
+BLOCKLIST_SOURCE_URL="$(curl -s https://www.iblocklist.com/lists.php | grep -o -e "http://list.iblocklist.com/[a-zA-Z\?\=;&0-9]*" | tr '\n' ' ')"
+# grab the lists
+for i $BLOCKLIST_SOURCE_URL; do
+    LISTNAME=$(echo $i | grep -o -E "list=[a-z]*" | grep -o -E "[a-z]*$") # list temp filename from uri
+    curl "$i" > "${LISTS_DIR}/${LISTNAME}.gz"                             # download the list
+    gunzip "${LISTS_DIR}/${LISTNAME}.gz"                                  # unpack the list
+    cat "${LISTS_DIR}/${LISTNAME}" >> "${LISTS_DIR}/cated.list"           # concat it in one file
 done
 
-if [[ ! -e final_list ]]; then
-    mkdir final_list
+if [[ ! -e "${DESTINATION_DIR}" ]]; then
+    mkdir "${DESTINATION_DIR}"
 fi
-# Filter duplicate entrys, so we only have a rule once in the final list
-uniq -i ${LISTS_DIR}/cated.list > final_list/${LISTDATE} 
-# add the dummyTitle for the Rule. Transmission seems to need this in order to be able to digest the list
-# That it is the same for all rules, seems not to matter. Rulenames are pointless with several 100k rules...
-sed 's/\(.*\)/dummyTitle:\1/g' final_list/${LISTDATE} > final_list/${LISTDATE}.tmp
-mv final_list/${LISTDATE}.tmp final_list/${LISTDATE}
-AMT_RULES_ORIG=$(wc -l ${LISTS_DIR}/cated.list | cut -f 3 -d ' ')
-AMT_RULES_new=$(wc -l final_list/${LISTDATE} | cut -f 3 -d ' ')
-
-echo "Originally ${AMT_RULES_ORIG} where merged to ${AMT_RULES_new} Rules. That saved " $(($AMT_RULES_ORIG-$AMT_RULES_new)) " duplicates."
-
-gzip -k final_list/${LISTDATE}
-gzip ${LISTS_DIR}/cated.list
-if [[ ! -e list_archive ]]; then
-    mkdir list_archive
-fi
-mv ${LISTS_DIR}/cated.list.gz list_archive/$LISTDATE.gz
-rm -Rf ${LISTS_DIR}
-HERE=$PWD
-cd "$TRANSMIT_BLOCKLIST_PATH"
-rm list.txt*
-cd "$HERE"
-mv final_list/${LISTDATE} "$TRANSMIT_BLOCKLIST_PATH/list.txt"
+# first uniq attempt
+uniq -i "${LISTS_DIR}/cated.list" > "${DESTINATION_DIR}/${LISTDATE}.uniq1"
+# second attempt with ip only
+for i in $(cut -d ':' -s -f 2 "${DESTINATION_DIR}/${LISTDATE}.uniq1" | uniq -i); do
+    echo "T:$i" >> "${DESTINATION_DIR}/${LISTDATE}.p2p"  # produce final p2p file
+done
+# create compressed version for archive purposes
+gzip -k "${DESTINATION_DIR}/${LISTDATE}.p2p"
+# rename to final filename
+mv "${DESTINATION_DIR}/${LISTDATE}.p2p" "${DESTINATION_DIR}/${OUTOUT_FINAL_FILENAME}"
+# delete stuff
+rm -Rf "${LISTS_DIR}"
+rm -Rf "${DESTINATION_DIR}/${LISTDATE}.uniq1"
 exit 0
